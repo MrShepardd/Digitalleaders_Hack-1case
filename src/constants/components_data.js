ebfc93a7d23825f5b7887cd1670c9e5c5a544598
Konstantin Travin <49562463+MrShepardd@users.noreply.github.com>
2020-10-12 01:22:44 +1000

    removal constant from PeopleFlowMap component, some refactoring

---
+++ src/constants/components_data.js
@@ -0,0 +1,11 @@
+const get_cluster_polygons = require('./get_cluster_polygons').default;
+
+const get_atm_location = require('./get_atm_location').default;
+
+const get_people_flow = require('./get_people_flow.json');
+
+module.exports = {
+    get_cluster_polygons,
+    get_atm_location,
+    get_people_flow
+};
